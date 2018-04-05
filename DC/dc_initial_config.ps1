aca13ae720ea27babd748618c8f0a767ff30bc18
Dave Brett <dave@bretty.me.uk>
2018-04-05 12:48:12 +0100

    Update dc_initial_config.ps1

--- DC/dc_initial_config.ps1
+++ DC/dc_initial_config.ps1
@@ -1,3 +1,9 @@
+# This script will configure a Windows Server 1709 Core Server to run as the DC in my lab
+# DO NOT run this unless you are happy with what it is doing
+# This enables me to quickly stand up a new Domain and have it configured how I want for my lab
+# Feel free to hack / edit / copy this script and use for your own environments
+# For too long I have had all my scripts on USB keys etc - hence me putting it up on GitHub
+
 # Disable IPv6
 $Adapter = Get-NetAdapter
 $NICName = $Adapter.Name
@@ -5,15 +11,18 @@ Disable-NetAdapterBinding -InterfaceAlias $NICName -ComponentID ms_tcpip6
 New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\services\TCPIP6\Parameters -Name DisabledComponents -PropertyType DWord -Value 0xffffffff
 
 # Rename Adapter
-rename-netadapter -name $NICName -newname "vlan_100"
+$VLANName
+rename-netadapter -name $NICName -newname $VLANName
 
 # Set Timezone
-set-timezone -name "GMT Standard Time"
+$TimeZone = "GMT Standard Time"
+set-timezone -name $TimeZone
 
 # Set Up New Domain
 $DomainName = "bretty.me.uk"
-Install-WindowsFeature AD-Domain-Services -includemanagementtools
 $Password = "ReallySecure"
+
+Install-WindowsFeature AD-Domain-Services -includemanagementtools
 $SecurePassword = ConvertTo-SecureString -AsPlainText $Password -Force
 Install-ADDSforest -DomainName $DomainName -InstallDNS -safemodeadministratorpassword $SecurePassword -force
 
@@ -53,6 +62,9 @@ Install-WindowsFeature ADCS-Web-Enrollment
 Install-AdcsWebEnrollment -Force
 
 # Generate Wildcard Certificate and put in DFS Share
+# Thanks to Martin for this script - slightly edited to suite my initial DC Build
+# http://citrixlab.dk/archives/544
+
 New-Item -ItemType Directory -Path C:\infrastructure\certificates
 New-Item -ItemType Directory -Path C:\infrastructure\certificates\internal
 New-Item -ItemType Directory -Path C:\infrastructure\certificates\internal\wildcard
@@ -100,3 +112,66 @@ Remove-Item -Path wildcard.req -Force
 Remove-Item -Path wildcard.rsp -Force
 Remove-Item -Path wildcard.ini -Force
         
+# Create OU Structure
+New-ADOrganizationalUnit -Name bretty -passthru
+New-ADOrganizationalUnit -Name "infrastructure" -Path "ou=bretty,dc=bretty,dc=me,dc=uk" -PassThru
+New-ADOrganizationalUnit -Name "users" -Path "ou=bretty,dc=bretty,dc=me,dc=uk" -PassThru
+New-ADOrganizationalUnit -Name "groups" -Path "ou=bretty,dc=bretty,dc=me,dc=uk" -PassThru
+New-ADOrganizationalUnit -Name "workers" -Path "ou=bretty,dc=bretty,dc=me,dc=uk" -PassThru
+
+# Set up Sites and Services
+Get-ADObject -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -filter "objectclass -eq 'site'" | Set-ADObject -DisplayName Liphook
+Get-ADObject -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -filter "objectclass -eq 'site'" | Rename-ADObject -NewName Liphook
+New-ADReplicationSubnet -Name "192.168.0.0/24" -Site Liphook
+New-ADReplicationSubnet -Name "192.168.1.0/24" -Site Liphook
+New-ADReplicationSubnet -Name "192.168.100.0/24" -Site Liphook
+New-ADReplicationSubnet -Name "192.168.101.0/24" -Site Liphook
+
+# Add UPN Suffix for Domain
+Get-ADForest | Set-ADForest -UPNSuffixes @{add="bretty.local"}
+
+# Install and Configure WSUS
+# Thanks Eric of XenApp Blog for this
+# https://xenappblog.com/2017/automate-wsus-on-windows-2016-server-core/
+
+Install-WindowsFeature -Name UpdateServices -IncludeManagementTools
+New-Item -Path E: -Name WSUS -ItemType Directory
+New-SmbShare -Name wsus$ -Path e:\wsus -FullAccess Administrators -ReadAccess Users
+New-DfsnFolder -Path \\$DomainName\public\wsus -TargetPath \\dc\wsus$
+CD "C:\Program Files\Update Services\Tools"
+.\wsusutil.exe postinstall CONTENT_DIR=E:\WSUS
+
+$wsus = Get-WSUSServer
+$wsusConfig = $wsus.GetConfiguration()
+Set-WsusServerSynchronization -SyncFromMU
+$wsusConfig.AllUpdateLanguagesEnabled = $false           
+$wsusConfig.SetEnabledUpdateLanguages("en")           
+$wsusConfig.Save()
+
+$subscription = $wsus.GetSubscription()
+$subscription.StartSynchronizationForCategoryOnly()
+
+Get-WsusServer | Get-WsusProduct | Where-Object -FilterScript { $_.product.title -match "Office" } | Set-WsusProduct -Disable
+Get-WsusServer | Get-WsusProduct | Where-Object -FilterScript { $_.product.title -match "Windows" } | Set-WsusProduct -Disable
+Get-WsusServer | Get-WsusProduct | Where-Object -FilterScript { $_.product.title -match "Windows Server 2016" } | Set-WsusProduct
+Get-WsusServer | Get-WsusProduct | Where-Object -FilterScript { $_.product.title -match "Language Packs" } | Set-WsusProduct -Disable
+
+Get-WsusClassification | Where-Object {$_.Classification.Title -in ('Critical Updates','Definition Updates','Feature Packs','Security Updates','Service Packs','Update Rollups','Updates')} | Set-WsusClassification
+
+$subscription.SynchronizeAutomatically=$true
+$subscription.SynchronizeAutomaticallyTimeOfDay= (New-TimeSpan -Hours 0)
+$subscription.NumberOfSynchronizationsPerDay=1
+$subscription.Save()
+$subscription.StartSynchronization()
+
+# Add CName for WSUS
+Add-DnsServerResourceRecordCName -Name "wsus" -HostNameAlias "dc.bretty.me.uk" -ZoneName "bretty.me.uk"
+
+# Stop First Run Wizard
+$wsusconfig.OobeInitialized = $true
+$wsusConfig.Save()
+
+
+
+
+
